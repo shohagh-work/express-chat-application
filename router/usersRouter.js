2c611cf014bbcf0b8575f307f1c819160b753b19
Shohagh <shohaghpikt@gmail.com>
2023-09-03 10:21:37 +0600

    up

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -8,8 +8,14 @@ const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse
 // initialization
 const router = express.Router();
 
-// login page
+// user page
 router.get("/", decorateHtmlResponse("Users"), getUsers);
+
+// add user
+router.post("/", decorateHtmlResponse("Users"), getUsers);
+
+// remove user
+router.delete("/", decorateHtmlResponse("Users"), getUsers);
 
 // exports
 module.exports = router;
