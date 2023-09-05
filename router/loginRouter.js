f5e6c950489a870e9129725d1583c43699af7711
shohaghpikt <85873344+shohaghpikt@users.noreply.github.com>
2023-09-05 16:32:16 +0600

    update from office

--- router/loginRouter.js
+++ router/loginRouter.js
@@ -8,8 +8,17 @@ const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse
 // initialization
 const router = express.Router();
 
+// set page title
+const page_title = "Login";
+
 // login page
-router.get("/", decorateHtmlResponse("Login"), getLogin);
+router.get("/", decorateHtmlResponse(page_title), getLogin);
+
+// process login
+router.post("/", decorateHtmlResponse(page_title), getLogin);
+
+// logout
+router.delete("/", getLogin);
 
 // exports
 module.exports = router;
