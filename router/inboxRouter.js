5a39d4399b0856bdcb4bf665e04e4e3446b2c0fd
shohaghpikt <85873344+shohaghpikt@users.noreply.github.com>
2023-09-12 17:05:00 +0600

    updated from office

--- router/inboxRouter.js
+++ router/inboxRouter.js
@@ -9,8 +9,20 @@ const { checkLogin } = require("../middlewares/common/checkLogin");
 // initialization
 const router = express.Router();
 
-// login page
+// inbox page
 router.get("/", decorateHtmlResponse("Inbox"),checkLogin, getInbox);
+
+// inbox page
+router.post("/search", decorateHtmlResponse("Inbox"),checkLogin, getInbox);
+
+// inbox page
+router.post("/conversation", decorateHtmlResponse("Inbox"),checkLogin, getInbox);
+
+// inbox page
+router.get("/messages/:conversation_id", decorateHtmlResponse("Inbox"),checkLogin, getInbox);
+
+// inbox page
+router.post("/message", decorateHtmlResponse("Inbox"),checkLogin, getInbox);
 
 // exports
 module.exports = router;
