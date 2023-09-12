1c0d7008b0c5409b2fdc6d220dd5a26135856565
shohaghpikt <85873344+shohaghpikt@users.noreply.github.com>
2023-09-12 17:35:22 +0600

    updated from office

--- models/Conversation.js
+++ models/Conversation.js
@@ -2,7 +2,33 @@
 const mongoose = require("mongoose");
 
 // create schema
+const conversationSchema = mongoose.Schema(
+    {
+        creator: {
+            id: mongoose.Types.ObjectId,
+            name: String,
+            avatar: String,
+        },
+
+        participant: {
+            id: mongoose.Types.ObjectId,
+            name: String,
+            avatar: String,
+        },
+
+        last_updated: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
 
 // create model
+const Conversation = mongoose.model("Conversation", conversationSchema);
 
 // exports
+module.exports = Conversation;
