864b2cd1180d508c76d963cfb9f4529faee0f51e
Shohagh <shohaghpikt@gmail.com>
2023-09-16 23:19:37 +0600

    updated from home

--- router/inboxRouter.js
+++ router/inboxRouter.js
@@ -2,27 +2,44 @@
 const express = require("express");
 
 // internal imports
-const { getInbox } = require("../controller/inboxController");
+const {
+  getInbox,
+  searchUser,
+  addConversation,
+  getMessages,
+  sendMessage,
+} = require("../controller/inboxController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
 const { checkLogin } = require("../middlewares/common/checkLogin");
+const attachmentUpload = require("../middlewares/inbox/attachmentUpload");
 
 // initialization
 const router = express.Router();
 
 // inbox page
-router.get("/", decorateHtmlResponse("Inbox"),checkLogin, getInbox);
+router.get("/", decorateHtmlResponse("Inbox"), checkLogin, getInbox);
+
+// search user for conversation
+router.post("/search", checkLogin, searchUser);
 
 // inbox page
-router.post("/search", decorateHtmlResponse("Inbox"),checkLogin, getInbox);
+router.post(
+  "/conversation",
+  decorateHtmlResponse("Inbox"),
+  checkLogin,
+  getInbox
+);
 
 // inbox page
-router.post("/conversation", decorateHtmlResponse("Inbox"),checkLogin, getInbox);
+router.get(
+  "/messages/:conversation_id",
+  decorateHtmlResponse("Inbox"),
+  checkLogin,
+  getInbox
+);
 
 // inbox page
-router.get("/messages/:conversation_id", decorateHtmlResponse("Inbox"),checkLogin, getInbox);
-
-// inbox page
-router.post("/message", decorateHtmlResponse("Inbox"),checkLogin, getInbox);
+router.post("/message", decorateHtmlResponse("Inbox"), checkLogin, getInbox);
 
 // exports
 module.exports = router;
